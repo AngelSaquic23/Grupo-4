07cd992c7bfe3963295d04b9f85c8dfe6fd2b916
Ludw1n <lajualipd@miumg.edu.gt>
2025-03-21 19:28:51 -0600

    Ludwin

---
+++ Ludwin.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+class LALADL{
+	private : 
+	string cui, nombres, apellidos;
+	
+	
+	void crear();
+	void leer();
+	void actualizar();
+	void borrar();
+};
