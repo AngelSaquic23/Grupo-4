12abdbb6f9f77b9819a5a24f84a34b2905ca6501
Angel Saquic <saquicangel7@gmail.com>
2025-04-05 12:28:15 -0600

    Programas de Angel Saquic

---
+++ Tablas_de_multiplicar.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Iterar sobre los números del 1 al 10
+    for (int i = 1; i <= 10; ++i) {
+        cout << "Tabla del " << i << ":\n";
+        
+        // Calcular la tabla de multiplicar para el número i
+        for (int j = 1; j <= 10; ++j) {
+            cout << i << " x " << j << " = " << i * j << endl;
+        }
+        
+        cout << endl; // Espacio entre tablas
+    }
+
+    return 0;
+}
