3c45bf844cb3cb0d2e5f0eb3d7fdf1296fed2af8
G-C-L-J <gaby.nativi14@gmail.com>
2025-04-05 16:26:33 -0600

    programa de Gabriela Alvarado

---
+++ cuarta ley.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    const float g = 9.81; // aceleración por gravedad en la Tierra (m/s²)
+    float masa, peso;
+
+    cout << "Cálculo del peso usando F = m * g" << endl;
+    cout << "Ingrese la masa del objeto (kg): ";
+    cin >> masa;
+
+    peso = masa * g;
+
+    cout << "El peso del objeto en la Tierra es: " << peso << " N" << endl;
+
+    return 0;
+}
+
