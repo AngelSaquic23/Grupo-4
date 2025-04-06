b3377ad15280b2f015d10aa47d4a79c870c85e98
PixelWizard <saquicangel7@gmail.com>
2025-04-05 18:43:42 -0600

    Update Tablas_de_multiplicar.cpp

--- Tablas_de_multiplicar.cpp
+++ Tablas_de_multiplicar.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 
 int main() {
-    // Iterar sobre los números del 1 al 10
+    // Iterar sobre los nÃºmeros del 1 al 10
     for (int i = 1; i <= 10; ++i) {
         cout << "Tabla del " << i << ":\n";
         
-        // Calcular la tabla de multiplicar para el número i
+        // Calcular la tabla de multiplicar para el nÃºmero i
         for (int j = 1; j <= 10; ++j) {
             cout << i << " x " << j << " = " << i * j << endl;
         }
@@ -14,5 +14,5 @@ int main() {
         cout << endl; // Espacio entre tablas
     }
 
-    return 0;
+     system("pause");
 }
